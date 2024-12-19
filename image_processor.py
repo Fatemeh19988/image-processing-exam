e5816bd5b9787faff6a66a5a48d0ad3496b71f5c
Fatemeh19988 <fatemh.reyhani.77@gmail.com>
2024-12-19 17:48:44 +0330

    Resize the image to 100x100 pixels

--- image_processor.py
+++ image_processor.py
@@ -10,11 +10,35 @@ image_path = os.path.join(desktop_path, 'image.jpg')
 # Read the image
 image = cv2.imread(image_path)
 
+# Get dimensions of the original image
+original_height, original_width = image.shape[:2]
+print(f'Original image dimensions: {original_width}x{original_height}')
+
 # Convert the image to grayscale
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Get dimensions of the grayscale image
+gray_height, gray_width = gray_image.shape[:2]
+print(f'Grayscale image dimensions: {gray_width}x{gray_height}')
 
 # Construct the full path to save the grayscale image
 gray_image_path = os.path.join(desktop_path, 'grayscale_image.jpg')
 
 # Save the grayscale image
 cv2.imwrite(gray_image_path, gray_image)
+
+# Resize the image to 100x100 pixels
+resized_image = cv2.resize(image, (100, 100))
+
+# Get dimensions of the resized image
+resized_height, resized_width = resized_image.shape[:2]
+print(f'Resized image dimensions: {resized_width}x{resized_height}')
+
+# Construct the full path to save the resized image
+resized_image_path = os.path.join(desktop_path, 'resized_sample.jpg')
+
+# Save the resized image
+if cv2.imwrite(resized_image_path, resized_image):
+    print(f'Resized image saved successfully at: {resized_image_path}')
+else:
+    print('Failed to save resized image.')
